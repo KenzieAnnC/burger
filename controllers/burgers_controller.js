ce16fb8d29ee56d2e72c1ec67e127d0281dc5dde
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-19 14:16:34 -0500

    adds burgers_controller code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,36 @@
 var burger = require("../models/burger.js");
 
+var express = require('express');
+var router = express.Router();
+
+router.get("/", function(request, response) {
+
+    burger.all(function(data) {
+      var hbsObj = {
+        burgers: data
+      };
+      response.render("index", hbsObj);
+    });
+  });
+  
+  router.post("/api/burgers/", function(request, response) {
+    
+    burger.createOne([
+      "burger_name"], [request.body.burger_name],
+      function(data) {
+      response.redirect("/");
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(request, response) {
+    
+    var condition = "id = " + request.params.id;
+    burger.updateOne({
+      isDevoured: true
+    }, condition, function(data) {
+      response.redirect("/");
+    })
+  });
+  
+  module.exports = router;
+
